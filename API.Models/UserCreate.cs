749dc7b44c9547ccc11e188347f2cd28cee45b49
Tessa Strube <70611318+tessstrube@users.noreply.github.com>
2020-10-20 10:38:14 -0500

    random commit

---
+++ API.Models/UserCreate.cs
@@ -0,0 +1,18 @@
+﻿using API.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class UserCreate
+    {
+        public Guid UserId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+    }
+}
