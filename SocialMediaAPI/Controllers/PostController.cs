3d4b76f0fb44746429ba11fe157d9450bf3707b9
DocMTyler <tylerhougland@att.net>
2020-10-20 16:10:40 -0400

    Adding a controller

---
+++ SocialMediaAPI/Controllers/PostController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using API.Data;
+using API.Service;
+
+namespace SocialMediaAPI.Controllers
+{
+    public class PostController : ApiController
+    {
+        public PostService CreatePostService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var postService = new PostService();
+            return postService;
+        }
+        
+        public IHttpActionResult Get()
+        {
+            PostService postService = CreatePostService();
+            var posts = postService.GetPosts();
+            return Ok(posts);
+        }
+    }
+}
