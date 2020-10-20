bc9956703b69b6afad0cf6c445ab79872a9d9176
Tessa Strube <70611318+tessstrube@users.noreply.github.com>
2020-10-20 10:21:42 -0500

    random commit

---
+++ API.Models/CommentCreate.cs
@@ -0,0 +1,16 @@
+﻿using API.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class CommentCreate
+    {
+        public int CommentId { get; set; }
+        public string Text { get; set; }
+        public User Author { get; set; }
+    }
+}
