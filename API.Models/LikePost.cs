86b8f9ef0bc3eeff02934d19d4b2f385459a2ba4
Tessa Strube <70611318+tessstrube@users.noreply.github.com>
2020-10-20 10:25:28 -0500

    random commit

---
+++ API.Models/LikePost.cs
@@ -0,0 +1,16 @@
+﻿using API.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class LikePost
+    {
+        public int PostId { get; set; }
+        public Post PostLike { get; set; }
+        public User Liker { get; set; }
+    }
+}
