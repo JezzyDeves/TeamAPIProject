233b811e7634333681c255c3cfcafbba111f1c05
DocMTyler <tylerhougland@att.net>
2020-10-20 14:12:45 -0400

    added like and comment

--- API.Service/PostService.cs
+++ API.Service/PostService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using API.Data;
+using API.Models;
+using SocialMediaAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,51 +9,69 @@ using System.Threading.Tasks;
 
 namespace API.Service
 {
-        public class PostService
+    public class PostService
+    {
+        private readonly Guid _userID;
+        private int PostID;
+
+        public bool CreatePost(PostCreate newPost)
         {
-            private readonly Guid _userID;
-            private int PostID;
-
-            public bool CreatePost(PostCreate newPost)
-            {
-                var entity =
-                    new Post()
-                    {
-                        OwnerID = _userID,
-                        Title = newPost.Title,
-                        Content = newPost.Text
-                    };
-
-                using (var ctx = new ApplicationDbContext())
+            var entity =
+                new Post()
                 {
-                    ctx.Posts.Add(entity);
-                    return ctx.SaveChanges() == 1;
-                }
+                    ID = PostID,
+                    Title = newPost.Title,
+                    Text = newPost.Text
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Posts.Add(entity);
+                return ctx.SaveChanges() == 1;
             }
-        public IENumerable<PostLineItem> GetPost()
-//        {
-//            using (var ctx = new ApplicationDbContext())
-//            {
-//                  {
-//                    var query =
-//                        ctx
-//                            .Posts
-//                                .Where(e => e.OwnerID == _userID)
-//                                .Select(
-//                                    e =>
-//                                    new PostLineItem()
-//                                      {
-//                                          PostID = e.PostID,
-//                                          Title = e.Title,
-//                                      }
-//                                  );
-//                   }
+        }
+        
+        public IEnumerable<PostDetail> GetPost()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                    var query =
+                        ctx
+                            .Posts
+                                .Where(e => e.OwnerID == _userID)
+                                .Select(e =>
+                                        new PostDetail()
+                                        {
+                                            PostID = e.PostID,
+                                            Title = e.Title,
+                                        }
+                                  );
 
+                    return query.ToArray();
+            }
+        }
 
-        //                   return query.ToArray();
-        //              }
+        public int LikePost(int postID, LikePost Like)
+        {
+            //using (var ctx = new ApplicationDbContext())
+            //Other stuff
+            return 1;
+        }
 
+        public string CommentPost(int postID, PostCommentDetail newPostComment)
+        {
+            var entity =
+                new Comment()
+                {
+                    PostID = postID,
+                    Text = newPostComment.Text,
+                };
 
-
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Posts.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
     }
 }
