4243c6289752f0978f6f4074eb051b7527d4fee2
DocMTyler <tylerhougland@att.net>
2020-10-20 13:42:41 -0400

    changes

---
+++ API.Service/PostService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Service
+{
+        public class PostService
+        {
+            private readonly Guid _userID;
+            private int PostID;
+
+            public bool CreatePost(PostCreate newPost)
+            {
+                var entity =
+                    new Post()
+                    {
+                        OwnerID = _userID,
+                        Title = newPost.Title,
+                        Content = newPost.Text
+                    };
+
+                using (var ctx = new ApplicationDbContext())
+                {
+                    ctx.Posts.Add(entity);
+                    return ctx.SaveChanges() == 1;
+                }
+            }
+        }
+}
