b66d1248568df51e45892d866564f33cd10e3c03
DocMTyler <tylerhougland@att.net>
2020-10-20 09:31:38 -0400

    New Day

---
+++ API.Models/CreatePost.cs
@@ -0,0 +1,18 @@
+﻿using API.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class CreatePost
+    {
+        Post post = new Post();
+        int id;
+        
+       
+    }
+}
