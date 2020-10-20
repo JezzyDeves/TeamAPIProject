cb7d8b556e90296c6fb2d366b5b5e7db9b2b2928
DocMTyler <tylerhougland@att.net>
2020-10-20 09:50:35 -0400

    Reply class add

---
+++ API.Data/Comment.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Win32;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class Reply : Comment
+    {
+        public string Comment { get; set;}
+    }
+}
