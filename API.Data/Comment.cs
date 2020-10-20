8bf5543fec04632ada7e2bb5239bb6a189af8e03
McCormickPMP <70542761+McCormickPMP@users.noreply.github.com>
2020-10-20 09:47:49 -0400

    first commit

---
+++ API.Data/Comment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    class Comment
+    {
+    }
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+        [Required]
+        public Guid OwnerId { get; set; }
+        [Required]
+        public int PostId { get; set; }
+        [ForeignKey(nameof(PostId))]
+        public virtual Post post { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public DateTimeOffset Created { get; set; }
+        [Required]
+        public DateTimeOffset? Modified { get; set; }
+    }
+}
---
+++ API.Data/Like.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class Like
+    {
+        [Required]
+        public int LikedPost { get; set; }
+
+        [ForeignKey(nameof(LikedPost))]//Should the foreign keys be under the Post aka General Store?
+        public virtual Post likedPost { get; set; }
+
+
+
+        public Guid Liker { get; set; }
+
+        [ForeignKey(nameof(Liker))]
+        public virtual User liker { get; set; }
+    }
+}
