fd0a663783f018a30166862c7ce215dcf3c81a8a
Tessa Strube <70611318+tessstrube@users.noreply.github.com>
2020-10-20 10:35:54 -0500

    random commit

---
+++ API.Models/PostDetail.cs
@@ -0,0 +1,14 @@
+﻿using API.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class PostDetail
+    {
+        public int PostId { get; set; }
+    }
+}
